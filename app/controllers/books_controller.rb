5e478244132e2adba1234e7dd88cb0c80262660d
koske0125 <79013492+koske0125@users.noreply.github.com>
2022-04-05 08:38:54 +0000

    [Update]Validatesの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,11 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-    redirect_to book_path(book.id) , notice: "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id) , notice: "Book was successfully created."
     else
       @books = Book.all
-      @book = Book.new
       render :index
     end
   end
@@ -25,9 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id) , notice: "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id) , notice: "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
