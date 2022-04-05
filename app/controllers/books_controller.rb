884a889b9e7a8edad2e309fbdb594fdc55b2c853
koske0125 <79013492+koske0125@users.noreply.github.com>
2022-04-05 07:41:11 +0000

    [Update]Flashmessageの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
+    if book.save
     redirect_to book_path(book.id) , notice: "Book was successfully created."
+    else
+      # @books = Book.all
+      # @book = Book.new
+      # render :index
+    end
   end
 
   def show
