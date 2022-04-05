4230a8c38984b7fcc10539c8dadcfa3317962abb
koske0125 <79013492+koske0125@users.noreply.github.com>
2022-04-05 07:03:17 +0000

    [Update]フラッシュメッセージを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id) , notice: "Book was successfully created."
   end
 
   def show
@@ -22,13 +22,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id) , notice: "Book was successfully updated."
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to '/books' , notice: "Book was successfully destroyed."
   end
 
   private
